13cb21390866a74f8c6692184f8970aad45c0c28
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-26 18:15:52 +0600

    last commit

--- practice.js
+++ practice.js
@@ -7,6 +7,18 @@ const loadDog = () =>{
 const displayDog = (dogList) =>{
     const main = document.getElementById('main');
     const first10Data = dogList.slice(0,10);
-    console.log(first10Data);
+   
+    for(const dog of first10Data){
+        const div = document.createElement('div');
+        div.className = "col-lg-4"
+        console.log(dog);
+        div.innerHTML = `
+          <h2>${dog.name}</h2>
+          <p> ${dog.temperament}</p>
+          <h4>${dog.weight.imperial}</h4>
+          <img width = "400px" height ="250px" src = "${dog.image.url}" />
+        `
+        main.appendChild(div)
+    }
 
 }
